7838f1e4149506a21526cbd503832f29b5f172ac
dalonsoperez <dalonsoperez@atanasoff23.danielcastelao.org>
2016-10-06 09:57:21 +0200

    cm²

--- NetBeansProjects/Rectangulo/src/rectangulo/Rectangulo.java
+++ NetBeansProjects/Rectangulo/src/rectangulo/Rectangulo.java
@@ -12,7 +12,7 @@ public class Rectangulo {
         float altura;
         base = Float.parseFloat(JOptionPane.showInputDialog("base "));
         altura = Float.parseFloat(JOptionPane.showInputDialog("altura "));
-                System.out.println("área rectangulo de base " + base + "cm e altura " + altura + "cm = " + base * altura + "cm");
+                System.out.println("área rectangulo de base " + base + "cm e altura " + altura + "cm = " + base * altura + "cm²");
     }
     
 }
