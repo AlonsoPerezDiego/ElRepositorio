b9754962d5773753459644301bad8e9ce6784bb2
dalonsoperez <dalonsoperez@atanasoff23.danielcastelao.org>
2016-10-06 10:16:07 +0200

    Segundo comentario

--- NetBeansProjects/Rectangulo/src/rectangulo/Rectangulo.java
+++ NetBeansProjects/Rectangulo/src/rectangulo/Rectangulo.java
@@ -14,6 +14,7 @@ public class Rectangulo {
         base = Float.parseFloat(JOptionPane.showInputDialog("base "));
         altura = Float.parseFloat(JOptionPane.showInputDialog("altura "));
                 System.out.println("área rectangulo de base " + base + "cm e altura " + altura + "cm = " + base * altura + "cm²");
+                //Despues de introducir los datos da el resultado 
     }
     
 }
